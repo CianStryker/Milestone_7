c612cd2ad7683cb7c6feca6ed7d7e2e1de4fffbe
CianStryker <cianstryker1@gmail.com>
2020-04-06 11:54:34 -0400

    Made some changes to the shiny app

--- Extensions/app.R
+++ Extensions/app.R
@@ -1,19 +1,58 @@
 
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("slate"),
 
-
+            navbarPage(
+                
+                title = "Replication Project",
+                
                 tabPanel("Extensions",
                          tags$iframe(style="height:1000px; width:100%; scrolling=yes",
-                                     src = "Replication_Main.pdf"))
+                                     src = "Extensions.pdf")),
+                
+                tabPanel("Rough Draft",
+                         tags$iframe(style="height:1000px; width:100%; scrolling=yes",
+                                     src = "Replication_Main.pdf")),
+                tabPanel(
+                    
+                    title = "About", 
+                    
+                    fluidRow(
+                        
+                        column(width = 3),
+                        
+                        column(width = 6,
+                               
+
+                               tags$h1("Original Paper"), 
+                               
+                               tags$p('The original paper for this replication project is "Ethnic Riots and Prosocial Behavior: Evidence from Kyrgyzstan" written by Anselm Hager, Krzysztof Krakowski, and Max Schaub.'), 
+                              
+                               tags$p("The original data can be found here :", a(href = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/WVBZNE", "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/WVBZNE")),
+                               
+                               tags$h1("About me"), 
+                               
+                               tags$p("My name is Cian Stryker and I am a graduate student at Harvard University. I am pursuing a Master’s in Russian, Eastern European, and Central Asian studies with a focus on the Digital Silk Road, or the technological aspects of the Belt and Road Initiative. I am especially interested in the digital expansion of Chinese technology and data management systems in Central Asia. This replication was the final project for my ‘Models’ class at Harvard (GOV 1006). "), 
+                               
+                               tags$h1("Source Code"), 
+                               
+                               tags$p("Here is the link to my Github page for this project:", a(href = "https://github.com/CianStryker/Milestone_7", "https://github.com/CianStryker/Milestone_7")) 
+                               
+                        ),
+                        
+                        column(width= 3)
+                    )
+                    
+                )
                         
                
 )
            
         
-    
+)    
 
 
 # Define server logic required to draw a histogram
